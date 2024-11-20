5c1020734e845570a1e9202c43900cb18de74362
Tianqing Liu <lhard@qq.com>
2024-11-20 11:34:47 +0800

    feat: 报表，完成

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,22 @@ import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
+function getCellColor(num: number) {
+  if (num > 0) {
+    return styles.cellTextGreen;
+  } else {
+    return styles.cellTextRed;
+  }
+}
+const Separator = () => <View style={styles.separator} />;
+
 function App(): React.JSX.Element {
+  // Mock数据
+  const tableData = [
+    { name: '北京', num: '3286', grew: 14 },
+    { name: '上海', num: '3178', grew: 46 },
+    { name: '郑州', num: '1895', grew: -24 },
+  ];
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -34,26 +49,62 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-            <Text>
-              Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-              screen and then come back to see your edits.
-            </Text>
+            <Text style={styles.label}>运营分析表</Text>
             {/* 表格报表 */}
             {/* 表格中的一行 */}
-            <View>
-              <View><Text>北京</Text></View>
-              <View><Text>3286.91</Text></View>
-              <View><Text>-14.5</Text></View>
-            </View>
+            {tableData.map(row => (
+              <View key={row.name} style={styles.tableRow}>
+                <View style={styles.tableCell}>
+                  <Text style={styles.tableCellText}>{row.name}</Text>
+                </View>
+                <View style={styles.tableCell}>
+                  <Text style={styles.tableCellText}>{row.num}</Text>
+                </View>
+                <View style={styles.tableCell}>
+                  <Text style={[styles.tableCellText, getCellColor(row.grew)]}>{row.grew}</Text>
+                </View>
+              </View>
+            ))}
         </View>
+
+        <Separator />
       </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  highlight: {
-    fontWeight: '700',
+  label: {
+    textAlign: 'center',
+    marginBottom: 5,
+    marginTop: 10,
+    fontSize: 24,
+  },
+  tableRow: {
+    flexDirection: 'row',
+    height: 30,
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  tableCell: {
+    flex: 1,
+  },
+  tableCellText: {
+    textAlign: 'center',
+  },
+  cellTextGreen: {
+    color: 'green',
+  },
+  cellTextRed: {
+    color: 'red',
+  },
+
+  separator: {
+    marginVertical: 8,
+    // color: '#fff',
+    borderBottomColor: '#737373',
+    // borderBottomColor: '#fff',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
 
