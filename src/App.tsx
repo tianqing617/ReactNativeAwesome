f982d392b2628be8e562aa2db91847fddc5c1d34
Tianqing Liu <lhard@qq.com>
2024-12-12 17:31:16 +0800

    feat: APP需要优化

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React from 'react';
-// import type {PropsWithChildren} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
+  // StyleSheet,
   useColorScheme,
-  View,
 } from 'react-native';
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
-import BasicChart from './echarts/BasicChart.tsx'
+import WelcomeScreen from './Welcome.tsx'
+import HomeScreen from './Home.tsx'
 
-function getCellColor(num: number) {
-  if (num > 0) {
-    return styles.cellTextGreen;
-  } else {
-    return styles.cellTextRed;
-  }
-}
-const Separator = () => <View style={styles.separator} />;
-
+const Stack = createNativeStackNavigator()
 function App(): React.JSX.Element {
-  // Mock数据
-  const tableData = [
-    { name: '北京', num: '3286', grew: 14 },
-    { name: '上海', num: '3178', grew: 46 },
-    { name: '郑州', num: '1895', grew: -24 },
-  ];
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -45,73 +39,28 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-            <Text style={styles.label}>运营分析表</Text>
-            {/* 表格报表 */}
-            {/* 表格中的一行 */}
-            {tableData.map(row => (
-              <View key={row.name} style={styles.tableRow}>
-                <View style={styles.tableCell}>
-                  <Text style={styles.tableCellText}>{row.name}</Text>
-                </View>
-                <View style={styles.tableCell}>
-                  <Text style={styles.tableCellText}>{row.num}</Text>
-                </View>
-                <View style={styles.tableCell}>
-                  <Text style={[styles.tableCellText, getCellColor(row.grew)]}>{row.grew}</Text>
-                </View>
-              </View>
-            ))}
-        </View>
-
-        {/* 分隔线 */}
-        <Separator />
-
-        {/* 图表 */}
-        <BasicChart />
+        {/* <Text>测试</Text> */}
+        <WelcomeScreen />
+        {/* 路由组件 */}
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen
+              name="Welcome"
+              component={WelcomeScreen}
+              options={{title: 'Welcome'}}
+            />
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  label: {
-    textAlign: 'center',
-    marginBottom: 5,
-    marginTop: 10,
-    fontSize: 24,
+/* const styles = StyleSheet.create({
+  highlight: {
+    fontWeight: '700',
   },
-  tableRow: {
-    flexDirection: 'row',
-    height: 30,
-    alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  tableCell: {
-    flex: 1,
-  },
-  tableCellText: {
-    textAlign: 'center',
-  },
-  cellTextGreen: {
-    color: 'green',
-  },
-  cellTextRed: {
-    color: 'red',
-  },
-
-  separator: {
-    backgroundColor: '#fff',
-    // paddingVertical: 8,
-    // color: '#fff',
-    borderBottomColor: '#737373',
-    // borderColor: '#fff',
-    paddingVertical: 8,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-});
+}); */
 
 export default App;
