448b9c978366c3950ca5c4ff58ee88b4fb36c016
Tianqing Liu <lhard@qq.com>
2025-01-06 11:36:32 +0800

    feat: 解决图片未加载问题，url路径不正确

--- src/views/image-manager/index.tsx
+++ src/views/image-manager/index.tsx
@@ -3,6 +3,7 @@ import {
   ScrollView,
   View,
   Image,
+  StyleSheet,
 } from 'react-native';
 import ICON_BASE64 from './icon-base64';
 
@@ -13,7 +14,7 @@ export default function App(): React.JSX.Element {
     >
       {/* 网络图片、本地图片、Base64图片，示例： */}
       {/* defaultSource: 加载图像源时显示的静态图像。 */}
-      <View>
+      <View style={styles.container}>
         <Image
           style={{width: 200, height: 200}}
           source={require('./insurance.jpeg')}
@@ -22,7 +23,7 @@ export default function App(): React.JSX.Element {
         {/* TODO: 未加载成功 */}
         <Image
           style={{width: 200, height: 200}}
-          src={'https://share.cleanshot.com/TxtbSk5d'}
+          src={'https://lsky.11101122.xyz/i/2024/09/30/66fa212771c19.png'}
           defaultSource={{uri: ICON_BASE64}}
         />
         <Image
@@ -34,8 +35,12 @@ export default function App(): React.JSX.Element {
   );
 }
 
-/* const styles = StyleSheet.create({
-  highlight: {
-    fontWeight: '700',
+const styles = StyleSheet.create({
+  container: {
+    height: 500,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 10,
   },
-}); */
+});
