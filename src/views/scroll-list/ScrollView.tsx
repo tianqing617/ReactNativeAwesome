8e3c2b243645edd210b391ac91b54ee97744f6c1
Tianqing Liu <lhard@qq.com>
2025-01-07 18:37:29 +0800

    feat: 完成scrollView示例

--- src/views/scroll-list/ScrollView.tsx
+++ src/views/scroll-list/ScrollView.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import {
+  StyleSheet,
   ScrollView,
   Text,
-  View,
+  Pressable,
 } from 'react-native';
 
 export default function App(): React.JSX.Element {
+  const NUM_ITEMS = 100;
+
+  const makeContent = (nItems: number, styles: any) => {
+    return Array(nItems)
+      .fill(1)
+      .map((_, i) => (
+        <Pressable
+          onLayout={() => console.log('layout:' + i)}
+          key={i}
+          style={styles}>
+          <Text>{'Item ' + i}</Text>
+        </Pressable>
+      ));
+  };
+
   return (
-    <ScrollView
-      contentInsetAdjustmentBehavior="automatic"
-    >
-      <View>
-        <Text>ScrollView Example</Text>
-      </View>
+    <ScrollView onScroll={e => console.log(e.nativeEvent.layoutMeasurement)}>
+      {makeContent(NUM_ITEMS, styles.itemWrapper)}
     </ScrollView>
   );
 }
 
-/* const styles = StyleSheet.create({
-  highlight: {
-    fontWeight: '700',
+const styles = StyleSheet.create({
+  itemWrapper: {
+    backgroundColor: '#dddddd',
+    alignItems: 'center',
+    borderRadius: 5,
+    borderWidth: 5,
+    borderColor: '#a52a2a',
+    padding: 30,
+    margin: 5,
   },
-}); */
+});
