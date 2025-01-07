0b29a825c04fe70ed909577ef15e938d591c57d7
Tianqing Liu <lhard@qq.com>
2025-01-07 18:45:41 +0800

    feat: 完成FlatList示例

--- src/views/scroll-list/FlatList.tsx
+++ src/views/scroll-list/FlatList.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
 import {
-  ScrollView,
+  StyleSheet,
   Text,
   View,
+  StatusBar,
+  FlatList,
 } from 'react-native';
 
-export default function App(): React.JSX.Element {
+interface PolicyItemType {
+  id: string
+  title: string
+}
+
+const NUM_ITEMS = 100;
+
+const DATA: PolicyItemType[] = Array(NUM_ITEMS).fill(1).map((_, i) => ({id: `id:${i}`,title: `id:${i}`}))
+
+const Item = ({title}: {title: string}) => {
   return (
-    <ScrollView
-      contentInsetAdjustmentBehavior="automatic"
-    >
-      <View>
-        <Text>FlatList</Text>
-      </View>
-    </ScrollView>
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+export default function App(): React.JSX.Element {
+  const renderItem = ({item}: { item: PolicyItemType }) => <Item title={item.title} />;
+
+  return (
+    <FlatList
+      debug={true}
+      data={DATA}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+    />
   );
 }
 
-/* const styles = StyleSheet.create({
-  highlight: {
-    fontWeight: '700',
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
-}); */
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
