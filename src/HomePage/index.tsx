5ad0921859f5c416e0dffd750481bd43d855a3a0
Tianqing Liu <lhard@qq.com>
2025-01-20 18:46:50 +0800

    feat: HomePage，使用react-query

--- src/HomePage/index.tsx
+++ src/HomePage/index.tsx
@@ -5,29 +5,36 @@ import {
   View,
   SafeAreaView,
 } from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import TopBar from './features/TopBar'
 
+const queryClient = new QueryClient({
+  // queries：指定与查询相关的默认配置。retry：定义查询失败时的重试次数。
+  defaultOptions: { queries: { retry: 2 }},
+})
 /**
  * function App(): React.JSX.Element：仅限制返回值为 JSX 元素
  * const App: React.FC：限定返回 JSX 元素并定义 props。此写法缺点是，还要再写一个export default
  */
 const App: React.FC = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      {/* 占位 */}
-      <View style={{height: 10}} />
-      {/* 顶栏 */}
-      <TopBar />
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView style={{flex: 1}}>
+        {/* 占位 */}
+        <View style={{height: 10}} />
+        {/* 顶栏 */}
+        <TopBar />
 
-      {/* 金刚位 + 瀑布流卡片 */}
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-      >
-        <View>
-          <Text>Blank Screen</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        {/* 金刚位 + 瀑布流卡片 */}
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+        >
+          <View>
+            <Text>Blank Screen</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
 export default App
