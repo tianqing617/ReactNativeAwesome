47027e5af73844f9cdf152e5578540dd89eba21e
Tianqing Liu <lhard@qq.com>
2025-01-23 12:00:47 +0800

    feat: HomePage，金刚位展示成功

--- src/HomePage/features/Icons/index.tsx
+++ src/HomePage/features/Icons/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-/* import {
+import React, { useState } from 'react';
+import {
   ScrollView,
-  Text,
   View,
-} from 'react-native'; */
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 import { queryIcons, IconType } from '../../api/homeAPI.tsx'
 import Grid from '../../components/Grid'
 import { NormalGridData } from '../../../views/grid-demo/config.ts'
@@ -40,17 +41,109 @@ export const queryRecyclerIcons = async (): Promise<RecyclerIcons> => {
     type: 'ICONS',
   }; */
 };
-// TODO: 实现金刚位，支持左、右横向滑动
 // queryRecyclerIcons()
 
-export default function App(): React.JSX.Element {
+// 金刚位，支持左、右横向滑动
+const wrapperHeight = 200;
+const windowWidth = Dimensions.get('window').width;
+
+export default function CateIcons(): React.JSX.Element {
+  // 金刚位 页数
+  const [indicator, setIndicator] = useState(0);
+
+  // TODO: width与height不确定
+  const height = 300
+  const width = windowWidth
+  const wrapperStyle = {height, width};
+
+  const indicatorHeight = 5;
+  const gridStyle = [styles.wrapper, {height: height - indicatorHeight, width}];
+
   return (
-    <Grid data={NormalGridData}></Grid>
+    <View style={wrapperStyle}>
+      <ScrollView
+        contentContainerStyle={styles.contentContainer}
+        horizontal
+        pagingEnabled
+        onMomentumScrollEnd={({nativeEvent}) => {
+          if (nativeEvent.contentOffset.x < windowWidth) {
+            setIndicator(0);
+          } else {
+            setIndicator(1);
+          }
+        }}
+        showsHorizontalScrollIndicator={false}
+      >
+        {/* 第一页 */}
+        <Grid
+          data={NormalGridData}
+          column={5}
+          style={gridStyle}
+          itemStyle={styles.itemStyle}
+          iconStyle={styles.iconStyle}
+          textStyle={styles.textStyle}
+        ></Grid>
+        {/* 第二页 */}
+        <Grid
+          data={NormalGridData}
+          column={5}
+          style={gridStyle}
+          itemStyle={styles.itemStyle}
+          iconStyle={styles.iconStyle}
+          textStyle={styles.textStyle}
+        ></Grid>
+      </ScrollView>
+      {/* 页数 */}
+      <View style={styles.indicatorBox}>
+        <View
+          style={indicator === 0 ? styles.activeIndicator : styles.indicator}
+        />
+        <View
+          style={indicator === 1 ? styles.activeIndicator : styles.indicator}
+        />
+      </View>
+    </View>
   );
 }
 
-/* const styles = StyleSheet.create({
-  highlight: {
-    fontWeight: '700',
+const styles = StyleSheet.create({
+  contentContainer: {
+    marginVertical: 12,
   },
-}); */
+  wrapper: {
+    width: windowWidth,
+    height: wrapperHeight, // 注意： wrapper.height > iconStyle.height*2
+  },
+  itemStyle: {
+    height: 80,
+  },
+  iconStyle: {
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+  },
+  textStyle: {
+    fontSize: 12,
+    color: '#636363',
+  },
+
+  indicatorBox: {
+    flexDirection: 'row',
+    height: 5,
+    alignSelf: 'center',
+    transform: [{translateY: -18}],
+  },
+  indicator: {
+    height: 5,
+    marginHorizontal: 5,
+    width: 8,
+    backgroundColor: '#A9A9A9',
+    borderRadius: 5,
+  },
+  activeIndicator: {
+    height: 5,
+    width: 15,
+    backgroundColor: '#FF4C39',
+    borderRadius: 5,
+  },
+});
