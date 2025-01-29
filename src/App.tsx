6af9c1e0937c9d212aeec9c7f19fc5629e247d59
Tianqing Liu <lhard@qq.com>
2025-01-30 00:13:47 +0800

    feat: Bottom Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import WelcomeScreen from './Welcome.tsx'
 import HomeScreen from './Home.tsx'
@@ -20,8 +21,27 @@ import FlatListDemo from './views/scroll-list/FlatList.tsx'
 // import RecycleListViewDemo from './views/scroll-list/RecycleListView.tsx'
 import HomePage from './HomePage/index.tsx'
 import GridPage from './views/grid-demo'
+// Navigation
+import DefaultPage from './views/navigation/DefaultPage.tsx'
 
 const Stack = createNativeStackNavigator()
+const BottomTab = createBottomTabNavigator();
+
+// 底部导航
+function BottomTabHome() {
+  return (
+    <BottomTab.Navigator
+      initialRouteName="My"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      {/* <BottomTab.Screen name="Home" component={TopTabHome} options={{title: '首页'}} /> */}
+      <BottomTab.Screen name="Messages" component={DefaultPage} options={{title: '消息'}}/>
+      <BottomTab.Screen name="My" component={DefaultPage} options={{title: '我'}}/>
+    </BottomTab.Navigator>
+  );
+}
+
 function App(): React.JSX.Element {
   // <Text>测试</Text>
   // <WelcomeScreen />
@@ -29,7 +49,18 @@ function App(): React.JSX.Element {
   <NavigationContainer>
     {/* Home */}
     {/* <Stack.Navigator initialRouteName="Home"> */}
-    <Stack.Navigator initialRouteName="HomePage">
+    <Stack.Navigator initialRouteName="BottomTabHome">
+      {/* 1.底部标签导航 */}
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name="BottomTabHome"
+        component={BottomTabHome}
+      />
+
+      {/* 2.顶部标签导航 */}
+      {/* 3.Page类页面 */}
       {/* TODO: 这里有类型问题，没有解决。去掉any可复现？ */}
       <Stack.Screen name="Home" component={HomeScreen as any} options={{title: 'Home'}} />
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
---
+++ src/views/navigation/DefaultPage.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import type { ParamListBase } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+export default function DefaultPage(
+  { route, navigation }: NativeStackScreenProps<ParamListBase>
+): React.JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>我是{route.name}页面</Text>
+      <Text
+        style={styles.button}
+        onPress={() => {
+          navigation.push('Welcome');
+        }}>
+        跳转下一页
+      </Text>
+      {/* TODO: 弹窗还未实现 */}
+      <Text
+        style={styles.button}
+        onPress={() => {
+          navigation.push('Modal');
+        }}>
+        打开弹窗
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    alignSelf: 'center',
+    marginVertical: 10,
+  },
+  button: {
+    alignSelf: 'center',
+    marginVertical: 10,
+    backgroundColor: '#0ac'
+  },
+});
